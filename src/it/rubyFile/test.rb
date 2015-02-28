ff442844c25a0a3a224c77d4d4d0e9286e094f55
yas99en <yas99en@gmail.com>
2015-02-28 12:58:38 +0900

    Modified test.rb.

--- src/it/rubyFile/test.rb
+++ src/it/rubyFile/test.rb
@@ -1,11 +1,13 @@
-$log.info("hello ruby from file");
-$log.info($project.name);
-$log.info($project.version);
-$log.info($project.basedir);
-$log.info($project.build.finalName);
-$log.info($project.build.directory);
-$log.info($project.build.outputDirectory);
-$log.info($settings.localRepository);
-$log.info($mvn.session.executionRootDirectory);
+$log.info "hello ruby from file"
+$log.info $project.name
+$log.info $project.version
+$log.info $project.basedir
+$log.info $project.build.finalName
+$log.info $project.build.directory
+$log.info $project.build.outputDirectory
+$log.info $settings.localRepository
+$log.info $mvn.session.executionRootDirectory
+$log.info $mvn.scriptFile
 p $0
+p $mvn.arguments
 p ARGV
