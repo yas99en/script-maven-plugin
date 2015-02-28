4e33318463bd491cdde9156a24c341147a90a192
yas99en <yas99en@gmail.com>
2015-02-28 12:27:40 +0900

    Added test.groovy and test.rb. Modified pom.xml and pom.xml.

---
+++ src/it/rubyFile/test.rb
@@ -0,0 +1,9 @@
+$log.info("hello from file");
+$log.info($project.name);
+$log.info($project.version);
+$log.info($project.basedir);
+$log.info($project.build.finalName);
+$log.info($project.build.directory);
+$log.info($project.build.outputDirectory);
+$log.info($settings.localRepository);
+$log.info($session.executionRootDirectory);
