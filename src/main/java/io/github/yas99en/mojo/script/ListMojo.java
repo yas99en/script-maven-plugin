0c96377015ed89fe89e260a8c2ff837738548522
yas99en <yas99en@gmail.com>
2015-03-04 21:32:24 +0900

    Added ListMojo.java.

---
+++ src/main/java/io/github/yas99en/mojo/script/ListMojo.java
@@ -0,0 +1,50 @@
+package io.github.yas99en.mojo.script;
+
+import java.util.List;
+
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "list")
+public class ListMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            listScriptEngines();
+        } catch (ScriptException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    private static void listScriptEngines() throws ScriptException {
+        ScriptEngineManager m = new ScriptEngineManager();
+
+        List<ScriptEngineFactory> factories = m.getEngineFactories();
+        for (ScriptEngineFactory factory: factories) {
+            System.out.println("    ---");
+            dumpEngineFactory(factory);
+        }
+        System.out.println("    ---");
+    }
+
+    private static void dumpEngineFactory(ScriptEngineFactory factory) {
+        System.out.println("    LanguageName   :"+factory.getLanguageName());
+        System.out.println("    LanguageVersion:"+factory.getLanguageVersion() );
+        System.out.println("    EngineName     :"+factory.getEngineName());
+        System.out.println("    EngineVersion  :"+factory.getEngineVersion());
+        System.out.println("    Extensions     :"+factory.getExtensions());
+        System.out.println("    MimeTypes      :"+factory.getMimeTypes());
+        System.out.println("    Names          :"+factory.getNames());
+    }
+
+    public static void main(String[] args) throws ScriptException {
+        listScriptEngines();
+    }
+}
