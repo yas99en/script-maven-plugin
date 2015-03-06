5b51888deb5ad52326499bffb0542fc44fa006c4
yas99en <yas99en@gmail.com>
2015-03-06 22:06:37 +0900

    Modified .travis.yml and singletest.sh.

--- singletest.sh
+++ singletest.sh
@@ -9,4 +9,5 @@ if [ $# -lt 1 ]; then
     usage
     exit 1
 fi
-exec mvn -Prun-its invoker:run -Dinvoker.test="$1"
+#exec mvn -Prun-its invoker:run -Dinvoker.test="$1"
+exec mvn -Prun-its -Dinvoker.test="$1" verify
