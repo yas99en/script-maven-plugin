639dee4d316b6474b914b6dfb8d5b3b339a16feb
yas99en <yas99en@gmail.com>
2015-03-08 22:45:55 +0900

    Modified ExecuteMojo.java.

--- src/main/java/io/github/yas99en/mojo/script/ExecuteMojo.java
+++ src/main/java/io/github/yas99en/mojo/script/ExecuteMojo.java
@@ -26,7 +26,7 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 @Mojo(name = "execute")
 public class ExecuteMojo extends AbstractMojo {
-    private static Pattern urlPattern = Pattern.compile("^jar:.*|^http://.*|^https://.*");
+    private static Pattern urlPattern = Pattern.compile("^jar:.*|^http://.*|^https://.*|^file:.*");
 
     @Parameter(defaultValue = "${session}", readonly = true)
     private MavenSession session;
