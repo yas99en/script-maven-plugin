8c9e88edfedef86225ea3b1f1719653be34cce01
yas99en <yas99en@gmail.com>
2015-03-22 11:31:36 +0900

    Modified EchoMojo.java.

--- src/main/java/io/github/yas99en/mojo/script/EchoMojo.java
+++ src/main/java/io/github/yas99en/mojo/script/EchoMojo.java
@@ -1,9 +1,13 @@
 package io.github.yas99en.mojo.script;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.PluginParameterExpressionEvaluator;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 
 /**
  * Echo message.
@@ -12,6 +16,12 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 @Mojo(name="echo")
 public final class EchoMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${session}", readonly = true)
+    private MavenSession session;
+    
+    @Parameter(defaultValue = "${mojoExecution}", readonly = true)
+    MojoExecution execution;
 
     @Parameter(property="scriptmvn.echo.messages")
     private String[] echoMessages;
@@ -22,6 +32,14 @@ public final class EchoMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         if(System.getProperty("scriptmvn.echo.output") != null) {
             echoOutput = System.getProperty("scriptmvn.echo.output");
+        }
+
+        PluginParameterExpressionEvaluator evaluator = new PluginParameterExpressionEvaluator(session, execution);
+        try {
+            Object obj = evaluator.evaluate(System.getProperty("scriptmvn.echo.message"));
+            echo(String.valueOf(obj));
+        } catch (ExpressionEvaluationException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
         }
 
         if(echoMessages != null) {
